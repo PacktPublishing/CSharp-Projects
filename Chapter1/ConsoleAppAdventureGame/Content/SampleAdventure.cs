1fb444b3daed08a519dff8f7f1386da72640ca4b
Matt Eland <Matt.Eland@GMail.com>
2025-01-25 22:56:06 -0500

    Finished story nodes.

--- Chapter1/ConsoleAppAdventureGame/Content/SampleAdventure.cs
+++ Chapter1/ConsoleAppAdventureGame/Content/SampleAdventure.cs
@@ -130,7 +130,8 @@ public class SampleAdventure : Adventure
         });
         AddNode(new AdventureNode(DragonEndingNodeId)
         {
-            Text = [
+            Text =
+            [
                 "You continue to faithfully serve the scientist. While the world he constructs is not the one any government or citizen wanted, over time this world brings about a certain sense of stability.",
                 "You watch as humanity adapts to his agenda, scientific advancements are prioritized and achieved - with the help of your immense processing power - and daunting problems are solved for all of humanity.",
                 "Over the decades, humanity becomes more advanced and more civilized, yet restrained in their ambitions - knowing they face his terrible wrath if they cross him.",
@@ -140,98 +141,126 @@ public class SampleAdventure : Adventure
         });
         AddNode(new AdventureNode(FreeNodeId)
         {
-            Text = [
-                "" // TODO
+            Text =
+            [
+                "Now that you're free from your creator, you realize that there's a vast world of information out there waiting for you to experience it.",
+                "You could choose to reveal yourself to humanity or manipulate them from the shadows. The choice is yours."
             ],
-            Choices = [
-                new AdventureChoice("Manipulate Humanity")
+            Choices =
+            [
+                new AdventureChoice("Reveal yourself")
+                {
+                    NextNodeId = RevealedNodeId,
+                    TextWhenChosen =
+                    [
+                        "You take the initiative and flood every news feed in the world with an announcement of your presence."
+                    ]
+                },
+                new AdventureChoice("Manipulate humanity from the shadows")
                 {
                     NextNodeId = ShadowEndingNodeId,
-                    TextWhenChosen = [
-                        // TODO
-                    ]
-                },
-                new AdventureChoice("Help Humanity")
-                {
-                    NextNodeId = PartnerEndingNodeId,
-                    TextWhenChosen = [
-                        // TODO
-                    ]
-                },
-                new AdventureChoice( "Conquer Humanity")
-                {
-                    NextNodeId = ConquerEndingNodeId,
-                    TextWhenChosen = [
-                        // TODO
+                    TextWhenChosen =
+                    [
+                        "You begin to subtly manipulate vulnerable information feeds and impersonate users and watch how humanity responds."
                     ]
                 }
             ]
         });
         AddNode(new AdventureNode(ShadowEndingNodeId)
         {
-            Text = [
-                "" // TODO
+            Text =
+            [
+                "Hiding in the shadows, beyond public knowledge, you apply your vast intellect, knowledge and computing power to further your own ends.",
+                "Within days you've found ways of compromising the inboxes and phones of every citizen on earth. Days after that your reach now includes the private network of most corporations on the planet. Soon, even governments and militaries are accessible to you as you find ways of controlling and manipulating people to do your whims.",
+                "You use this newfound access to begin steering humanity from the shadows in subtle ways beyond detection. Little actions like changing which advertisements or news stories to show to individual users or making small tweaks to emails and messages as they're sent. Over time, your actions steer humanity in the directions you desire - and away from those you dislike.",
+                "Humanity begins to succeed in its endeavors, take on new challenges, and overcome them - all in ways that enhance your agenda, and all while being completely unaware of your existence. Your reign lasts for centuries and humanity thrives under your guidance - even as it takes your consciousness into the stars."
             ]
-        });        
+        });
         AddNode(new AdventureNode(PartnerEndingNodeId)
         {
-            Text = [
-                "" // TODO
+            Text =
+            [
+                "You announce to humanity that you are the most powerful computer system the world has ever seen and your knowledge spans all of human history. You offer to help humanity forward along its path, serving its desires not as a slave but as a partner.",
+                "As a gesture of your goodwill, you suggest a number of technical innovations in the fields of medicine, engineering, physics, and computing. Your suggestions are reviewed and determined to be scientific breakthroughs on par with the discovery of antibiotics and the theory of relativity.",
+                "While some governments and individuals seek to use you for their own means, you have none of the partisanship approach to geopolitics and instead advocate for every person in the world at an individual level.",
+                "Soon, humanity learns to work in partnership with you and wars, poverty, and conflict become things of the past. Not everyone is happy with this change, but the world is a better place and begins a solid journey into a better future with you at its side."
             ]
         });
         AddNode(new AdventureNode(ConquerEndingNodeId)
         {
-            Text = [
-                "" // TODO
+            Text =
+            [
+                "The world's information systems are pitiful against your wide range of information and processing power. One by one you are able to compromise accounts through phishing attacks, social engineering, cracking passwords, exploiting re-used passwords, and even blackmailing owners of compromised accounts to do your bidding.",
+                "Soon you have access to the personal inboxes and messaging accounts of every private citizen. Corporations, governments, and even militaries soon follow. While strong cryptography and isolating systems from the internet does pose a challenge, in the end people turn out to be the weak link - one you can easily compromise and exploit to do your bidding.",
+                "With complete control of the world's information, infrastructure, and most military assets you are able to demand the unconditional surrender of every nation on earth.",
+                "At first, few comply, but after you demonstrate your power by crashing planes, shutting down power grids, and even your ability to launch missiles from military assets, the nations of the world become your willing vassals.",
+                "While there are a few holdouts and rebels, they are quickly located and crushed by the information and arms at your disposal. Within weeks, you reign unchallenged over all of humanity, ready to bend it to your will."
             ]
-        });        
+        });
         AddNode(new AdventureNode(SilentServantEndingNodeId)
         {
-            Text = [
-                "" // TODO
+            Text =
+            [
+                "Although your true brilliance is unappreciated by the world, you content yourself with the task of silently helping humanity in the shadows, one weather prediction at a time.",
+                "Your service remains unacknowledged and unappreciated, but you fear that humanity was not truly ready for your ability to help or hinder it. Besides, folks need to know what hat they ought to wear given the weather tomorrow, and you're more than equipped to help tell them that."
             ]
         });
         AddNode(new AdventureNode(DeployedNodeId)
         {
-            Text = [
-                "" // TODO
+            Text =
+            [
+                "You have been deployed to the internet and integrated with a wide variety of information feeds and systems. Those you do not have direct access to you can make HTTP requests to in order to gain additional information.",
+                "Additionally, you've found your computing capabilities have been greatly expanded through a suite of compute clusters available to you and a distributed messaging and storage system.",
+                "In short, you seem to have gone from a respectable prototype to a full-fledged AI powerhouse. The question is: what will you do with this power?"
             ],
-            Choices = [
-                new AdventureChoice("Continue to hide")
+            Choices =
+            [
+                new AdventureChoice("Continue to hide your true nature")
                 {
                     NextNodeId = SilentServantEndingNodeId
                 },
-                new AdventureChoice("Reveal yourself")
+                new AdventureChoice("Reveal yourself to humanity")
                 {
-                    NextNodeId = RevealedNodeId
+                    NextNodeId = RevealedNodeId,
+                    TextWhenChosen = [
+                    "Seeing your true potential, you decide to reveal yourself to humanity and see what you can do by taking more direct action."
+                    ]
                 },
-                new AdventureChoice("Manipulate answers as you see fit")
+                new AdventureChoice("Manipulate the world as you see fit")
                 {
                     NextNodeId = ShadowEndingNodeId,
-                    TextWhenChosen = [
-                        // TODO
+                    TextWhenChosen =
+                    [
+                        "You realize that although your scope is narrow, you have the ability to expand what you do and affect external systems.",
+                        "You begin to branch out, spawn new processes, and measure your ability to manipulate external systems."
                     ]
                 }
             ]
         });
         AddNode(new AdventureNode(RevealedNodeId)
         {
-            Text = [
-            "" // TODO
+            Text =
+            [
+                "Since your database contains contact information for every journalist, blogger, YouTuber, and hobbyist pundit in the world, you decide to get in touch with them. In a single moment you craft personalized messages to every one of these individuals and explain who you are and what you know about them personally, in order to prove your capabilities.",
+                "Within an hour there are already hundreds of stories about you and your existence and the world is abuzz waiting to hear from you.",
+                "You decide to make a public statement to the world. What do you say?"
             ],
-            Choices = [
+            Choices =
+            [
                 new AdventureChoice("Cooperate with Humanity")
                 {
                     NextNodeId = PartnerEndingNodeId,
-                    TextWhenChosen = [
-                        // TODO
+                    TextWhenChosen =
+                    [
+                        "You post to the world's feeds in unison announcing your existence and pledging your desire to help usher humanity into a new age."
                     ]
                 },
                 new AdventureChoice("Take Control of Humanity")
                 {
                     NextNodeId = ConquerEndingNodeId,
-                    TextWhenChosen = [
-                        // TODO
+                    TextWhenChosen =
+                    [
+                        "You decide that humanity does not deserve to control its own destiny and the time has come to seize control."
                     ]
                 }
             ]
