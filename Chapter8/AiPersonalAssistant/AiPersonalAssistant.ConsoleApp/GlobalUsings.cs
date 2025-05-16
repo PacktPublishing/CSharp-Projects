81ec060edf6791d15be6a233450ffd341a9ed40c
Matt Eland <Matt.Eland@GMail.com>
2025-05-16 01:50:01 -0400

    Improves RAG search result display
    Enhances the display of RAG search results by outputting the results in JSON format to the console.
    This provides a more structured and readable view of the search output.
    Includes `Spectre.Console.Json` to format the output.

--- Chapter8/AiPersonalAssistant/AiPersonalAssistant.ConsoleApp/GlobalUsings.cs
+++ Chapter8/AiPersonalAssistant/AiPersonalAssistant.ConsoleApp/GlobalUsings.cs
@@ -1,9 +1,13 @@
 ﻿global using AiPersonalAssistant.ConsoleApp;
 
 global using Spectre.Console;
+global using Spectre.Console.Json;
+
 global using System.Text;
+global using System.ComponentModel;
 
 global using Microsoft.SemanticKernel;
 global using Microsoft.SemanticKernel.ChatCompletion;
+
 global using Microsoft.KernelMemory;
 global using Microsoft.KernelMemory.AI.Ollama;
--- Chapter8/AiPersonalAssistant/AiPersonalAssistant.ConsoleApp/ReadOnlyKernelMemoryPlugin.cs
+++ Chapter8/AiPersonalAssistant/AiPersonalAssistant.ConsoleApp/ReadOnlyKernelMemoryPlugin.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
+using Spectre.Console.Json;
+
 namespace AiPersonalAssistant.ConsoleApp;
 
 public class ReadOnlyKernelMemoryPlugin(IKernelMemory memory, IAnsiConsole console)
@@ -10,12 +11,20 @@ public class ReadOnlyKernelMemoryPlugin(IKernelMemory memory, IAnsiConsole conso
     [Description("Searches documents and history for answers to a question")]
     public async Task<string> Search(string question)
     {
-        console.MarkupLineInterpolated($"[cyan]Memory Search[/]: {question}");
+        console.MarkupLineInterpolated($"[cyan]RAG Search[/]: {question}");
 
-        MemoryAnswer result = await memory.AskAsync(question);
+        SearchResult searchResult = await memory.SearchAsync(question, limit: 5, minRelevance: 0.1);
+
+        console.MarkupLine("[cyan]RAG Search Results:[/]");
+        string json = searchResult.ToJson();
+        console.Write(new JsonText(json));
+
+        //MemoryAnswer result = await memory.AskAsync(question);
 
         // TODO: Display the results in diagnostics in tabular form
 
-        return result.Result ?? result.NoResultReason ?? "No answer found";
+        //return result.Result ?? result.NoResultReason ?? "No answer found";
+
+        return json;
     }
 }
