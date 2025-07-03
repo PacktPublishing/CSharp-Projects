1d83895d8857e22ac5a95a06ed52434ace901232
Matt Eland <Matt.Eland@GMail.com>
2025-07-02 22:25:59 -0400

    Paddle class

---
+++ Chapter12/PingPongMono/Paddle.cs
@@ -0,0 +1,31 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PingPongMono;
+
+public class Paddle(int x, int y, int width, int height)
+{
+    private const int PaddleSpeed = 5;
+    public Rectangle Bounds { get; private set; } = new(x, y, width, height);
+    public Color Color { get; init; } = Color.White;
+    public required Keys UpKey { get; init; }
+    public required Keys DownKey { get; init; }
+
+    public void Update(KeyboardState keyboard, int maxY)
+    {
+        if (keyboard.IsKeyDown(UpKey) && Bounds.Y > 0)
+        {
+            Bounds = new Rectangle(Bounds.X, Bounds.Y - PaddleSpeed, Bounds.Width, Bounds.Height);
+        }
+        else if (keyboard.IsKeyDown(DownKey) && Bounds.Y < maxY - Bounds.Height)
+        {
+            Bounds = new Rectangle(Bounds.X, Bounds.Y + PaddleSpeed, Bounds.Width, Bounds.Height);
+        }
+    }
+    
+    public void Draw(SpriteBatch spriteBatch, Texture2D texture)
+    {
+        spriteBatch.Draw(texture, Bounds, Color);
+    }
+}
--- Chapter12/PingPongMono/PingPongGame.cs
+++ Chapter12/PingPongMono/PingPongGame.cs
@@ -10,7 +10,9 @@ public class PingPongGame : Game
     private readonly GraphicsDeviceManager _graphics;
     private SpriteBatch? _spriteBatch;
 
-    private Rectangle _paddle1, _paddle2, _ball;
+    private Paddle _paddle1;
+    private Paddle _paddle2;
+    private Rectangle _ball;
     private Vector2 _ballVelocity;
     private int _score1;
     private int _score2;
@@ -21,7 +23,6 @@ public class PingPongGame : Game
     private const int PaddleWidth = 10;
     private const int PaddleHeight = 60;
     private const int BallSize = 10;
-    private const int PaddleSpeed = 5;
     private const int BallSpeed = 4;
 
     public PingPongGame()
@@ -32,13 +33,23 @@ public class PingPongGame : Game
             PreferredBackBufferHeight = 480
         };
         Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+        IsMouseVisible = false;
     }
 
     protected override void Initialize()
     {
-        _paddle1 = new Rectangle(30, 210, PaddleWidth, PaddleHeight);
-        _paddle2 = new Rectangle(760, 210, PaddleWidth, PaddleHeight);
+        _paddle1 = new Paddle(30, 210, PaddleWidth, PaddleHeight)
+        {
+            UpKey = Keys.W,
+            DownKey = Keys.S,
+            Color = Color.MediumPurple
+        };
+        _paddle2 = new Paddle(Width - 30, 210, PaddleWidth, PaddleHeight)
+        {
+            UpKey = Keys.Up,
+            DownKey = Keys.Down,
+            Color = Color.AliceBlue
+        };
         _ball = new Rectangle(395, 235, BallSize, BallSize);
         _ballVelocity = new Vector2(BallSpeed, BallSpeed);
         base.Initialize();
@@ -48,36 +59,20 @@ public class PingPongGame : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         //_font = Content.Load<SpriteFont>("ScoreFont");
-        
+
         _whiteTexture = new Texture2D(GraphicsDevice, 1, 1);
         _whiteTexture.SetData([Color.White]);
     }
 
     private int Width => _graphics.PreferredBackBufferWidth;
     private int Height => _graphics.PreferredBackBufferHeight;
-    
+
     protected override void Update(GameTime gameTime)
     {
         KeyboardState keyboard = Keyboard.GetState();
 
-        // Player 1 (W/S)
-        if (keyboard.IsKeyDown(Keys.W) && _paddle1.Y > 0)
-        {
-            _paddle1.Y -= PaddleSpeed;
-        }
-        else if (keyboard.IsKeyDown(Keys.S) && _paddle1.Y < Height - PaddleHeight)
-        {
-            _paddle1.Y += PaddleSpeed;
-        }
-
-        // Player 2 (Up/Down)
-        if (keyboard.IsKeyDown(Keys.Up) && _paddle2.Y > 0)
-        {
-            _paddle2.Y -= PaddleSpeed;
-        } else if (keyboard.IsKeyDown(Keys.Down) && _paddle2.Y < Height - PaddleHeight)
-        {
-            _paddle2.Y += PaddleSpeed;
-        }
+        _paddle1.Update(keyboard, Height);
+        _paddle2.Update(keyboard, Height);
 
         // Move ball
         _ball.X += (int)_ballVelocity.X;
@@ -85,15 +80,17 @@ public class PingPongGame : Game
 
         // Ball collision with top/bottom
         if (_ball.Y <= 0 || _ball.Y >= Height - BallSize)
+        {
             _ballVelocity.Y *= -1;
+        }
 
         // Ball collision with paddles
-        if (_ball.Intersects(_paddle1) && _ballVelocity.X < 0)
+        if (_ball.Intersects(_paddle1.Bounds) && _ballVelocity.X < 0)
         {
             _ballVelocity.X *= -1;
         }
 
-        if (_ball.Intersects(_paddle2) && _ballVelocity.X > 0)
+        if (_ball.Intersects(_paddle2.Bounds) && _ballVelocity.X > 0)
         {
             _ballVelocity.X *= -1;
         }
@@ -104,6 +101,7 @@ public class PingPongGame : Game
             _score2++;
             ResetBall();
         }
+
         if (_ball.X > _graphics.PreferredBackBufferWidth)
         {
             _score1++;
@@ -122,12 +120,12 @@ public class PingPongGame : Game
     {
         _ball.X = Width / 2 - BallSize / 2;
         _ball.Y = Height / 2 - BallSize / 2;
-        
+
         bool isGoingLeft = Random.Shared.Next(2) == 0;
         bool isGoingUp = Random.Shared.Next(2) == 0;
-        
-        _ballVelocity = new Vector2(BallSpeed * (isGoingLeft ? -1 : 1), 
-                                    BallSpeed * (isGoingUp ? -1 : 1));
+
+        _ballVelocity = new Vector2(BallSpeed * (isGoingLeft ? -1 : 1),
+            BallSpeed * (isGoingUp ? -1 : 1));
     }
 
     protected override void Draw(GameTime gameTime)
@@ -136,8 +134,8 @@ public class PingPongGame : Game
 
         _spriteBatch!.Begin();
 
-        _spriteBatch.Draw(_whiteTexture, _paddle1, Color.MediumPurple);
-        _spriteBatch.Draw(_whiteTexture, _paddle2, Color.LightSteelBlue);
+        _paddle1.Draw(_spriteBatch, _whiteTexture!);
+        _paddle2.Draw(_spriteBatch, _whiteTexture!);
         _spriteBatch.Draw(_whiteTexture, _ball, Color.Yellow);
 
         // Draw score
